724817a3af76abcee180d5d39d4a9af5feaed00d
marcromeyn <marcromeyn@gmail.com>
2015-11-01 15:00:21 +0100

    Added 10 test diarree

--- PattRecClasses/record/record.m
+++ PattRecClasses/record/record.m
@@ -2,17 +2,22 @@ fs = 44100;
 bits = 16;
 recObj = audiorecorder(fs, bits, 1);
 
-%# Record voice for 2 seconds.
-disp('Start speaking.')
-recordblocking(recObj, 2);
-disp('End of Recording.');
+for n = 1:10
+    %# Record voice for 2 seconds.
+    disp('Start speaking.')
+    recordblocking(recObj, 2);
+    disp('End of Recording.');
+    fileName = strcat(num2str(n), '.wav');
+    audiowrite(strcat('Sounds/Test/diarree/', fileName), myRecording, fs);
+    disp('Wait for it...');
+    pause(1);
+end
 
 %# Play back the recording.
-play(recObj);
-
-myRecording = getaudiodata(recObj);
+% play(recObj);
+% 
+% myRecording = getaudiodata(recObj);
 
 %# Plot the waveform.
 plot(myRecording);
 
-audiowrite('Sounds/Test/diabetes/10.wav', myRecording, fs);
